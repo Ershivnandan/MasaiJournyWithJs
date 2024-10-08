183d6b6f65b3195b9cca1f48372c23691ad2d833
ErShivnandan <ec1912@global.org.in>
2024-10-09 01:18:34 +0530

    qu4 solution added

---
+++ Fun with Arrays I Adding Removing and Finding Items/q4.js
@@ -0,0 +1,40 @@
+function highestPaid(emp){
+    var heighestPaid
+
+    heighestPaid = emp.reduce((prev, curr)=> {
+        return (prev.salary > curr.salary) ? prev : curr;
+    })
+
+    return heighestPaid
+}
+
+
+function destructuringToSwap(emp){
+    
+    var ans = emp.reduce((acc, curr, index, arr)=> {
+        if(index == 0){
+            acc.push(arr[arr.length -1]);
+        }
+        else if (index === arr.length -1){
+            acc.push(arr[0])
+        }
+        else{
+            acc.push(curr)
+        }
+        return acc
+    }, [])
+
+    return ans
+}
+
+
+const employees = [
+  { name: "John Doe", age: 30, department: "HR", salary: 50000 },
+  { name: "Jane Smith", age: 28, department: "Finance", salary: 60000 },
+  { name: "Alex Johnson", age: 35, department: "IT", salary: 70000 },
+];
+
+//console.log(highestPaid(employees)); // Output: { name: 'Alex Johnson', age: 35, department: 'IT', salary: 70000 }
+
+
+console.log(destructuringToSwap(employees));
